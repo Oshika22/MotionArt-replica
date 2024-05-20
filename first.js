0bc6763d01012ee9a70651f3b68c554f60a62693
Oshika22 <oshika004@gmail.com>
2024-05-20 22:30:12 +0530

    edited cursor using

--- first.js
+++ first.js
@@ -1,10 +1,32 @@
-console.log("hello");
 
-const element = document.querySelector(".row");
+const main =  document.querySelector(".body-content");
+const curs1 = document.querySelector("#cursor1");
+const curs2 = document.querySelector("#cursor2");
 
-element.addEventListener("mousemove", (e) => {
-    const { x, y } = element.getBoundingClientRect();
-    element.style.setProperty("--x", e.clientX - x);
-    element.style.setProperty("--y", e.clientY - y);
 
+main.addEventListener("mousemove",function(dets){
+    gsap.to(curs1,{
+        x: dets.x,
+        y: dets.y,
+        scale: 1,
+        background: "whitesmoke",
+    })
+    gsap.to(curs2,{
+        x: dets.x,
+        y: dets.y,
+        scale: 5,
+        background: "radial-gradient( whitesmoke, pink,rgb(239, 160, 173),rgb(228, 133, 149),rgb(221, 64, 153),whitesmoke )",
+    })
+    gsap.to(curs2,{
+        x: dets.x,
+        y: dets.y,
+        duration: 2,
+        scale: 0,
+    })
+    gsap.to(curs1,{
+        x: dets.x,
+        y: dets.y,
+        duration: 1.75,
+        scale: 0,
+    })
 })
